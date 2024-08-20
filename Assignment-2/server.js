1f6965406b68f4a739522069c44b566a2e3ac5a8
Lijo S <112559369+lijo-1@users.noreply.github.com>
2024-08-20 17:58:41 +0530

    Update server.js

--- Assignment-2/server.js
+++ Assignment-2/server.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+const data = {
+    "id": 1,
+    "title": "Inception",
+    "director": "Christofer Nolan",
+    "year": 2010,
+    "poster": "Inception.jpeg"
+};
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('static'));
+
+app.get("/", (req, res) => {
+    res.render("forms.ejs", { data: data });  // Pass data to the template
+});
+
 console.log("Server is running")
